72e4fe0983749fb23aca762ec795164f518966c2
albercik420 <100830575+alb3rcik@users.noreply.github.com>
2022-03-10 07:21:40 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CrookedMediocreHashfunction/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+int liczba1, liczba2, wynik;
+ printf("Wprowadz pierwsza liczbe: ");
+scanf("%d", &liczba1);
+printf("Wprowadz druga liczbe: ");
+scanf("%d", &liczba2);
+wynik = liczba1 * liczba2;
+printf("Pole prostokąta wynosi %d", wynik);
+ if (liczba1 == liczba2 ){
+    printf("To jest kwadrat"}
+return 0; 
+} 
